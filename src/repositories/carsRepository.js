07cf6332c05638992c82770b795755490131981b
franciscoeymardneto <francisco.eymard.oliveira@gmail.com>
2023-12-08 20:10:48 -0300

    chore: create cars repository

---
+++ src/repositories/carsRepository.js
@@ -0,0 +1,24 @@
+import { readFile, writeFile } from "node:fs/promises";
+export default class CarsRepository {
+  constructor({ file }) {
+    this.file = file;
+  }
+
+  async #readFileContent() {
+    return JSON.parse(await readFile(this.file));
+  }
+
+  async read() {
+    return await this.#readFileContent();
+  }
+
+  async create({ data }) {
+    const currentData = await this.#readFileContent();
+
+    currentData.push(data);
+
+    await writeFile(this.file, JSON.stringify(currentData));
+
+    return data;
+  }
+}
